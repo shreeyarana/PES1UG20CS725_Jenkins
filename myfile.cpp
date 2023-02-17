7647301b4ee1c9f3a39307cfefa9e8918c307ab6
sr01 <101698720+shreeyarana@users.noreply.github.com>
2023-02-17 15:07:48 +0530

    Create myfile.cpp

---
+++ myfile.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "This is being done using Jenkinsfile\n";
+  cout << "I have successfully built and run\n";
+  cout << "Name:Shreeya rana P\tSRN:PES1UG20CS725\n";
+  return 0;
+}
